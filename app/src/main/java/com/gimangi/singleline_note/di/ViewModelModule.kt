f5173ca5dcf94e7d671f22cbea29bfc4ff276d78
gimangi <khk211113@naver.com>
2022-05-08 23:50:59 +0900

    #12 [FEAT] di with koin

--- app/src/main/java/com/gimangi/singleline_note/di/ViewModelModule.kt
+++ app/src/main/java/com/gimangi/singleline_note/di/ViewModelModule.kt
@@ -1,4 +1,10 @@
 package com.gimangi.singleline_note.di
 
-class ViewModelModule {
+import com.gimangi.singleline_note.viewmodel.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val ViewModelModule = module {
+
+    viewModel { MainViewModel(get()) }
 }
