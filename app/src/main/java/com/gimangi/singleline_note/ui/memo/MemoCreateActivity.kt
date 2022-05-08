cb5cc791da4f69b989d5fbd0ec6ff976e3830e66
gimangi <khk211113@naver.com>
2022-05-09 03:51:30 +0900

    #23 [FEAT] button enable

--- app/src/main/java/com/gimangi/singleline_note/ui/memo/MemoCreateActivity.kt
+++ app/src/main/java/com/gimangi/singleline_note/ui/memo/MemoCreateActivity.kt
@@ -2,7 +2,10 @@ package com.gimangi.singleline_note.ui.memo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import androidx.core.widget.addTextChangedListener
 import com.gimangi.singleline_note.R
 import com.gimangi.singleline_note.databinding.ActivityMemoCreateBinding
 import com.gimangi.singleline_note.ui.base.BaseActivity
@@ -17,5 +20,35 @@ class MemoCreateActivity :
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        initBinding()
+        initTextWatcher()
+        initClickListener()
+    }
+
+    private fun initBinding() {
+        binding.viewModel = memoCreateViewModel
+    }
+
+    private fun initTextWatcher() {
+        binding.etNewMemoTitle.editText.addTextChangedListener(AllTextNotEmptyWatcher())
+        binding.etNewMemoSuffix.editText.addTextChangedListener(AllTextNotEmptyWatcher())
+    }
+
+    inner class AllTextNotEmptyWatcher : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+        override fun afterTextChanged(p0: Editable?) {
+            memoCreateViewModel.allTextNotEmpty.set(
+                binding.etNewMemoTitle.editText.text.isNotEmpty() && binding.etNewMemoSuffix.editText.text.isNotEmpty()
+            )
+        }
+    }
+
+    private fun initClickListener() {
+        binding.ibToolbarBack.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/gimangi/singleline_note/ui/memo/MemoCreateViewModel.kt
+++ app/src/main/java/com/gimangi/singleline_note/ui/memo/MemoCreateViewModel.kt
@@ -1,6 +1,9 @@
 package com.gimangi.singleline_note.ui.memo
 
+import androidx.databinding.ObservableField
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MemoCreateViewModel : ViewModel() {
+    val allTextNotEmpty = ObservableField(false)
 }
